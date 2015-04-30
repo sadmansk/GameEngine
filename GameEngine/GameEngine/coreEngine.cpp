49e77859cc356b5457f6b81509a58c5ecc3996d4
Sadman Kazi <sadmansazidk@gmail.com>
2015-04-30 16:14:55 -0400

    Added a class to keep track of time and updated license andreadme

--- GameEngine/GameEngine/coreEngine.cpp
+++ GameEngine/GameEngine/coreEngine.cpp
@@ -1,3 +1,18 @@
+/*
+A test game engine with C++ and OpenGL
+Copyright (C) 2015  Sadman Kazi
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 #include "coreEngine.h"
 
 CoreEngine::CoreEngine(std::string title, int width, int height) : title(title), width(width), height(height)
